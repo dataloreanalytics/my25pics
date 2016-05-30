f0d07a16fb08511116fb4d2e92a374018837e1cb
Mohit Kishore <gkmohit@gmail.com>
2016-05-29 20:48:58 -0400

    Trying to get smaller thumbnails, removed cmments

--- js/FBGetPhotos.js
+++ js/FBGetPhotos.js
@@ -6,7 +6,6 @@ function getAllPhotosAndLikes(callback){
             'GET',
             {"fields":"albums.limit(999999){name,count,id,photos.limit(999999){id,created_time,name,images,likes.limit(999999)}}","limit":"999999"},
             function(response) {
-               // Insert your code here
                console.log(response);
                callback(response);
             }
@@ -21,14 +20,8 @@ function getPhotosFromAlbum(allAlbum){
    for(var i = 0; i < allAlbum.length; i++){
       if( "photos" in allAlbum[i]){
          for(var j = 0; j < allAlbum[i].photos.data.length; j++){
-            // console.log(allAlbum[i].photos.data[j]);
             var photo = allAlbum[i].photos.data[j];
             var photoObj;
-            // console.log(photo);
-            // console.log(photo.created_time);
-            // console.log(photo.id);
-            // console.log(photo.images);
-            // allPhotos.push
             var likes = 0;
             var name = "";
             if( "likes" in photo){
@@ -39,8 +32,6 @@ function getPhotosFromAlbum(allAlbum){
             }
             photo.likes = likes;
             photo.name = name;
-            // console.log(photo.likes);
-            // console.log(photo.name);
             photoObj = {
                'created_time' : photo.created_time,
                'photo_id'	   : photo.id,
@@ -49,7 +40,6 @@ function getPhotosFromAlbum(allAlbum){
                'name'         : photo.name,
             }
             allPhotos.push(photoObj);
-            // console.log(allPhotos.length);
          }
       }
    }
@@ -59,41 +49,22 @@ function getPhotosFromAlbum(allAlbum){
 
 function login(){
    getAllPhotosAndLikes(function(response){
-
-      // var top_25_img = $('.top-25-img');
-      // var top_25_lbox = $('.top-25-lbox');
       var div = $("#links");
-      // setTimeout(function(){
-      //
-      //    // console.log(JSON.stringify(response));
-      // }, 1000);
 
       console.log(response.albums.data.length);
       console.log(response.albums);
       var allPhotos = getPhotosFromAlbum(response.albums.data);
-      // console.log(allPhotos);
-      // console.log(allPhotos[2]);
-      // console.log(allPhotos[3]);
-      // console.log(JSON.stringify(allPhotos[1]));
       var sortedPhotos = _.sortBy( allPhotos, 'likes' ).reverse();
-      // console.log(sortedPhotos);
-      // console.log(sortedPhotos);
       for(i = 0; i< sortedPhotos.length && i < 25 ; i++){
          var thumbNail = sortedPhotos[i].images[sortedPhotos[i].images.length - 1].source;
          var highResImg = sortedPhotos[i].images[0].source;
          var name = sortedPhotos[i].name;
          console.log(sortedPhotos[i]);
-         console.log(name);
-         console.log(highResImg);
-         console.log(thumbNail);
-         // var images = ["\"img/1.jpg\"", "\"img/3.jpg\"", "\"img/3.jpg\""];
-         // for(var i = 0; i < 3; i++){
          var a =  '<a href=\"' + highResImg +  '\" title='+ '"' + name  + '" ' +'data-gallery> ';
          var img = '<img src=\"' + thumbNail + '\" alt='+ '"' + name  + '" ' +'> </a>';
          var append = a + img;
          var append = append;
-         div.append(append).fadeIn(2000);
-         // }
+         div.append(append).fadeIn(3000);
       }
       setTimeout(function(){
          $('#FBAuthorized').fadeIn();
