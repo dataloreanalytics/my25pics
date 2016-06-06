f001a9020b33673b1f729a340b97d73913748ca3
Mohit Kishore <gkmohit@gmail.com>
2016-06-06 00:09:23 -0400

    Version 1.2 - Changed br loation 3

--- js/signInAndSave.js
+++ js/signInAndSave.js
@@ -38,7 +38,7 @@ function signInAndSave(){
                var a =  '<a href=\"' + highResImg +  '\" title='+ '"' + name  + '" ' +'data-gallery> ';
                var img = '<img  src=\"' + thumbNail + '\" alt='+ '"' + name  + '" ' +' style="max-height:150px"> </a>';
                var append = a + img;
-               if( i == 3 || i == 8 || i == 13 || i == 18 || i == 23 ){
+               if( i == 4 || i == 9 || i == 14 || i == 19 || i == 24){
                   console.log(i);
                   append = append + '<br>';
                }
