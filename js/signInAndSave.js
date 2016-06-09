e6d44e2c04d5cdb3692062193b8035df684c00a3
Mohit Kishore <gkmohit@gmail.com>
2016-06-09 00:34:38 -0400

    v 1.2 added thank you payment bug fixes 3

--- js/signInAndSave.js
+++ js/signInAndSave.js
@@ -59,7 +59,7 @@ function signInAndSave(){
             writeUserData(userId, response, top_25) ;
             $("#loginRow").fadeOut(200);
             $("#logoutRow").fadeIn(1000);
-
+            orderId(userId, top_25);
             // createAlertDiv("Succesfully logged in", true);
          }
 
@@ -190,5 +190,5 @@ function orderId(userId, top25pics){
    var link="https://app.moonclerk.com/pay/gkq6vcpdo5p?cid=" + orderId;
    var moonclerk = '<a href="' + link + '"'  + 'class="btn btn-warning" role="button">Checkout my 25 pics</a>';
    checkout.append(moonclerk);
-   
+
 }
