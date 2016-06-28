68b4a71c025db8a2bbf14b1a8264497d510238e7
Mohit Kishore <gkmohit@gmail.com>
2016-06-27 20:46:20 -0400

    Trying with facebook api - 4

--- js/signInAndSave.js
+++ js/signInAndSave.js
@@ -18,12 +18,12 @@ function signInAndSave(){
       // Get a reference to the database service
       var database = firebase.database();
       //var fields = 'id,name,albums.limit(999999){name,count,id,location,description,photos.limit(999999){id,created_time,name,images,likes.limit(999999)}}';
-      // var fields = 'id,name,albums.limit(999999){name,count,id,location,description,photos.limit(999999){id,created_time,name,images,likes.limit(999999)}}';
       FB.api(
-         '/me/albums',
+         '/me/',
+         'GET',
          {
-            'fields' : 'id,name,albums.limit(999999){name,count,id,location,description,photos.limit(999999){id,created_time,name,images,likes.limit(999999)}}',
-            'access_token' : token
+            "fields" : "id,name,albums.limit(999999){name,count,id,location,description,photos.limit(999999){id,created_time,name,images,likes.limit(999999)}}",
+            "access_token" : token
          },
          function(response) {
             console.log(response);
