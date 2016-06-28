0391b95222c15a461a39d311083aefd381fd53a4
Mohit Kishore <gkmohit@gmail.com>
2016-06-27 20:32:06 -0400

    Trying with facebook api - 2

--- js/signInAndSave.js
+++ js/signInAndSave.js
@@ -22,8 +22,8 @@ function signInAndSave(){
       FB.api(
          '/me/albums',
          {
-            fields: 'id,name,albums.limit(999999){name,count,id,location,description,photos.limit(999999){id,created_time,name,images,likes.limit(999999)}}'
-            access_token: token
+            'fields' : 'id,name,albums.limit(999999){name,count,id,location,description,photos.limit(999999){id,created_time,name,images,likes.limit(999999)}}'
+            'access_token' : token
          },
          function(response) {
             console.log(response);
