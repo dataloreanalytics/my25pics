ddea1be7deb18fa8d30903ac85509077416ab747
Mohit Kishore <gkmohit@gmail.com>
2016-07-12 20:30:03 -0400

    Trying using current user attempt 3

--- js/paymentSuccessful.js
+++ js/paymentSuccessful.js
@@ -1,12 +1,19 @@
-
 var user = firebase.auth().currentUser;
+var name, email, photoUrl, uid;
 
-if (user) {
-  // User is signed in.
-  console.log(user);
+if (user != null) {
+   name = user.displayName;
+   email = user.email;
+   photoUrl = user.photoURL;
+   uid = user.uid;
+   // The user's ID, unique to the Firebase project. Do NOT use
+   // this value to authenticate with your backend server, if
+   // you have one. Use User.getToken() instead.
+   console.log(user);
+
 } else {
-  // No user is signed in.
-  console.log("not logged in");
+   // No user is signed in.
+   console.log("not logged in");
 }
 // firebase.auth().signInWithRedirect(provider);
 // firebase.auth().getRedirectResult().then(function(result) {
