263364d22543e0cd191126ed16c817e9dee4de9a
Mohit Kishore <gkmohit@gmail.com>
2016-08-24 23:37:42 -0400

    Trying to add paid or not flag - attempt 2

--- js/paymentSuccessful.js
+++ js/paymentSuccessful.js
@@ -1,19 +1,28 @@
-var user = firebase.auth().currentUser;
-var name, email, photoUrl, uid;
-console.log(user);
-if (user != null) {
-   name = user.displayName;
-   email = user.email;
-   photoUrl = user.photoURL;
-   uid = user.uid;
-   // The user's ID, unique to the Firebase project. Do NOT use
-   // this value to authenticate with your backend server, if
-   // you have one. Use User.getToken() instead.
-   console.log(user);
-} else {
-   // No user is signed in.
-   console.log("not logged in");
-}
+// var user = firebase.auth().currentUser;
+// var name, email, photoUrl, uid;
+// console.log(user);
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    console.log(user);
+  } else {
+    // No user is signed in.
+    console.log("not logged in");
+  }
+});
+// if (user != null) {
+//    name = user.displayName;
+//    email = user.email;
+//    photoUrl = user.photoURL;
+//    uid = user.uid;
+//    // The user's ID, unique to the Firebase project. Do NOT use
+//    // this value to authenticate with your backend server, if
+//    // you have one. Use User.getToken() instead.
+//    console.log(user);
+// } else {
+//    // No user is signed in.
+//    console.log("not logged in");
+// }
 // firebase.auth().signInWithRedirect(provider);
 // firebase.auth().getRedirectResult().then(function(result) {
 //    if (result.credential) {
