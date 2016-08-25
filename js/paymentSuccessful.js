719fdaf1e0f304b2fb3f3d054ca2ee870a2808f8
Mohit Kishore <gkmohit@gmail.com>
2016-08-25 00:59:57 -0400

    Writing to the DB 9

--- js/paymentSuccessful.js
+++ js/paymentSuccessful.js
@@ -1,15 +1,25 @@
 firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    var userId = user.uid;
-    var orderDate = getTodayDatePath();
-    firebase.database().ref('orders/' + orderDate + '/' + userId).set({
-      'paid' : "true",
-   });
-  } else {
-    // No user is signed in.
-    console.log("not logged in");
-  }
+   if (user) {
+      // User is signed in.
+      var userId = user.uid;
+      var orderDate = getTodayDatePath();
+      firebase.database().ref('orders/' + orderDate + '/' + userId + '/paid').set({
+         'paid' : "true",
+      });
+
+      // Get a key for a new Post.
+      // var newPostKey = firebase.database().ref().child('orders/' + orderDate + '/' + userId + '/paid').push().key;
+      // console.log(newPostKey);
+      // Write the new post's data simultaneously in the posts list and the user's post list.
+      // var updates = {};
+      // updates['/posts/' + newPostKey] = postData;
+      // updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+      //
+      // return firebase.database().ref().update(updates);
+   } else {
+      // No user is signed in.
+      console.log("not logged in");
+   }
 });
 
 function getTodayDatePath(){
@@ -21,4 +31,9 @@ function getTodayDatePath(){
    var hh = today.getHours();
    var orderDate = yyyy + '/' + mm + '/' + dd;
    return orderDate;
+}
+function getMonthNames(month){
+   var monthNames = ["January", "February", "March", "April", "May", "June",
+   "July", "August", "September", "October", "November", "December"];
+   return monthNames[month];
 }
