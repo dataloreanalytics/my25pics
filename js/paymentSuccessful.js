a9d94024f08f19b007c63ccbe672adf44e201158
Mohit Kishore <gkmohit@gmail.com>
2016-08-25 01:20:10 -0400

    Writing to the DB - atempt 15

--- js/paymentSuccessful.js
+++ js/paymentSuccessful.js
@@ -11,7 +11,6 @@ firebase.auth().onAuthStateChanged(function(user) {
       var newPostKey = firebase.database().ref().child('orders/' + orderDate + '/' + userId + '/paid').push().key;
       console.log(newPostKey);
       // Write the new post's data simultaneously in the posts list and the user's post list.
-       var updates = {"paid" : "yes"};
       updates['orders/' + orderDate + '/' + userId + '/paid'] = "true";
       // updates['/user-posts/' + uid + '/' + newPostKey] = postData;
       //
